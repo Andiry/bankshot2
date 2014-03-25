2d3c9d6f7edd9daaf587408c7540a1ecf07dde19
Andiry Xu <jix024@cs.ucsd.edu>
2014-03-24 23:17:32 -0700

    Refine setup-cache.sh

--- kernel/setup-cache.sh
+++ kernel/setup-cache.sh
@@ -4,8 +4,17 @@ rmmod bankshot2
 
 sleep 1
 
+echo "load bankshot2 kernel module..."
 insmod bankshot2.ko phys_addr=0x100000000 cache_size=0x1000000
 #mount -t pmfs -o physaddr=0x100000000,init=2G,backing_dev=/dev/ram0 none /mnt/ramdisk
+
+sleep 1
+echo "Make XFS on bankshot2 block device.."
+mkfs.xfs /dev/bankshot2Block0
+
+sleep 1
+echo "Mount to /mnt/ramdisk.."
+mount /dev/bankshot2Block0 /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
