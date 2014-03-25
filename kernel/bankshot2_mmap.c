bebdebe0e33375d5a9ff8efa7b7936d94b5038bc
Andiry Xu <jix024@cs.ucsd.edu>
2014-03-25 10:29:01 -0700

    Add vm_mmap_pgoff

--- kernel/bankshot2_mmap.c
+++ kernel/bankshot2_mmap.c
@@ -1,7 +1,29 @@
 #include <linux/mman.h>
 #include <linux/audit.h>
+#include <linux/security.h>
 #include <linux/hugetlb.h>
 #include "bankshot2.h"
+
+unsigned long vm_mmap_pgoff(struct file *file, unsigned long addr,
+		unsigned long len, unsigned long prot,
+		unsigned long flag, unsigned long pgoff)
+{
+	unsigned long ret;
+	struct mm_struct *mm = current->mm;
+	unsigned long populate;
+
+//	ret = security_mmap_file(file, prot, flag);
+	ret = 0;
+	if (!ret) {
+		down_write(&mm->mmap_sem);
+		ret = do_mmap_pgoff(file, addr, len, prot, flag, pgoff,
+					&populate);
+		up_write(&mm->mmap_sem);
+		if (populate)
+			mm_populate(ret, populate);
+	}
+	return ret;
+}
 
 unsigned long bankshot2_mmap(unsigned long addr, unsigned long len,
 		unsigned long prot, unsigned long flags,
@@ -10,15 +32,15 @@ unsigned long bankshot2_mmap(unsigned long addr, unsigned long len,
 	struct file *file = NULL;
 	unsigned long retval = -EBADF;
 
-	audit_mmap_fd(fd, flags);
+//	audit_mmap_fd(fd, flags);
 	file = fget(fd);
 	if (!file)
 		goto out;
-	if (is_file_hugepages(file))
-		len = ALIGN(len, huge_page_size(hstate_file(file)));
+//	if (is_file_hugepages(file))
+//		len = ALIGN(len, huge_page_size(hstate_file(file)));
 	retval = -EINVAL;
-	if (unlikely(flags & MAP_HUGETLB && !is_file_hugepages(file)))
-		goto out_fput;
+//	if (unlikely(flags & MAP_HUGETLB && !is_file_hugepages(file)))
+//		goto out_fput;
 
 	//FIXME: ignore MAP_HUGETLB;
 
@@ -26,7 +48,7 @@ unsigned long bankshot2_mmap(unsigned long addr, unsigned long len,
 
 	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
 
-out_fput:
+//out_fput:
 	if (file)
 		fput(file);
 out:
