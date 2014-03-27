fa45abbbee9ad6241c42a38ae6a668c8a8638876
Andiry Xu <jix024@cs.ucsd.edu>
2014-03-27 11:20:33 -0700

    Add test_mmap.c.
    Currently we need:
    1. A user space library in libnvm.
    2. A inode number translation.
    3. A file size update (perhaps).

---
+++ test_mmap.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <malloc.h>
+
+#include "kernel/bankshot2_cache.h"
+
+int main(void)
+{
+	int fd, fd1;
+	struct bankshot2_cache_data data;
+	struct bankshot2_mmap_request mmap1;
+	void *addr;
+	int rnw = 1;
+	char *buf;
+
+	buf = malloc(4096);
+	memset(buf, 'c', 4096);
+
+	fd1 = open("/mnt/ramdisk/test1", O_RDWR | O_CREAT, 0640);
+	data.file = fd1;
+	data.offset = 0;
+	data.size = 4096;
+	data.rnw = READ_EXTENT;
+	data.read = (rnw == READ_EXTENT);
+	data.write = (rnw == WRITE_EXTENT);
+
+	mmap1.fd = fd1;
+	mmap1.addr = NULL;
+	mmap1.length = 4096;
+	mmap1.prot = PROT_WRITE;
+	mmap1.flags = MAP_SHARED;
+	mmap1.offset = 0;
+
+	fd = open("/dev/bankshot2Ctrl0", O_RDWR);
+	printf("fds: %d %d\n", fd1, fd);
+	ioctl(fd, BANKSHOT2_IOCTL_CACHE_DATA, &data);
+
+	addr = mmap(NULL, 4096, PROT_WRITE, MAP_SHARED, fd1, 0);
+	printf("mmap addr: \t%p\n", addr);
+	memset(buf, 'd', 4096);
+	memcpy(addr, buf, 4096);
+	munmap(addr, 4096);
+	ioctl(fd, BANKSHOT2_IOCTL_MMAP_REQUEST, &mmap1);
+	addr = (void *)mmap1.mmap_addr;
+	printf("mmap1 addr: \t%p\n", addr);
+
+	memset(buf, 'c', 4096);
+	memcpy(addr, buf, 4096);
+
+	munmap(addr, 4096);
+	close(fd);
+	close(fd1);
+	free(buf);
+
+	return 0;
+}
