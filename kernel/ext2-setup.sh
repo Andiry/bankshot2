ee746a4494a9379fd302f4c0e17346b7085f6368
Andiry Xu <jix024@cs.ucsd.edu>
2014-06-01 17:16:47 -0700

    Use /dev/ram0 as default backing store

--- kernel/ext2-setup.sh
+++ kernel/ext2-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 umount /mnt/ramdisk
-mkfs.ext2 -b 4096 /dev/memuram0
+mkfs.ext2 -b 4096 /dev/ram1
 
 sleep 1
 echo "Mount to /mnt/ramdisk.."
-mount /dev/memuram0 /mnt/ramdisk
+mount /dev/ram1 /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
