6af9c8a7da5bb58db15229ee7526daffa29bc171
Andiry Xu <jix024@cs.ucsd.edu>
2014-06-06 09:56:54 -0700

    Update ext2-setup.sh to work with /dev/ram1

--- kernel/ext2-setup.sh
+++ kernel/ext2-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-umount /mnt/ramdisk
+umount /mnt/ramdisk1
 mkfs.ext2 -b 4096 /dev/ram1
 
 sleep 1
-echo "Mount to /mnt/ramdisk.."
-mount /dev/ram1 /mnt/ramdisk
+echo "Mount to /mnt/ramdisk1.."
+mount -o noatime /dev/ram1 /mnt/ramdisk1
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
