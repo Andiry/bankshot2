f71de3d411002d0b3f3017eacd2ec0c65c6b3a41
Andiry Xu <jix024@cs.ucsd.edu>
2014-06-07 18:04:47 -0700

    xdd.py for multithreads

--- paper/Graphs/xdd.py
+++ paper/Graphs/xdd.py
@@ -15,7 +15,9 @@ f = open(filename, 'r')
 round = 0
 types = []
 traces = []
-data=[[] for i in range(2)]
+data1 = [[] for i in range(2)]
+data4 = [[] for i in range(2)]
+data16 = [[] for i in range(2)]
 
 for line in f:
 	line = line.rstrip()
@@ -23,43 +25,93 @@ for line in f:
 	if round == 0:
 		for j in range(2,len(parts)):
 			types.append(parts[j])
-	else:
+	elif int(parts[1]) == 1:
 		traces.append(parts[0])
 		for j in range(2,len(parts)):
-			data[j - 2].append(float(parts[j]))
+			data1[j - 2].append(float(parts[j]) / 1000)
+	elif int(parts[1]) == 4:
+		for j in range(2,len(parts)):
+			data4[j - 2].append(float(parts[j]) / 1000)
+	elif int(parts[1]) == 16:
+		for j in range(2,len(parts)):
+			data16[j - 2].append(float(parts[j]) / 1000)
 	round += 1
 
 file.close(f)
 print types
 print traces
-print data[0]
+print data1[0]
+print data4[0]
+print data16[0]
 
 N = len(traces)
 ind = np.arange(N)
 width = 0.2
 
 print ind
-print len(data[0])
+print len(data1[0])
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
+ax = fig.add_subplot(131)
 rects = [[] for i in range(2)]
 color_pattern = ['1','0', '0.7', '0.3','0.6','0.9', '0.2', '0.5', '0.8', '0.1', '0.4','0.7', '1'] #light and contrasting colors
 hatch_pattern = ['\\','|','.','*','+','/', ' ', 'x', ' ', '-'] 
 for j in range(2):
     name = 'rects' + str(j)
 #    rects[j] = ax.bar(ind + width * j, data[j], width, color=color_pattern[j % len(color_pattern)], hatch = hatch_pattern[j % len(hatch_pattern)])
-    rects[j] = ax.bar(ind + width * (j + 1), data[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
+    rects[j] = ax.bar(ind + width * (j + 1), data1[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
 
 # add some
-ax.set_ylabel('Bandwidth (MB/s)', fontsize = '40')
+ax.set_ylabel('Bandwidth (GB/s)', fontsize = '30')
 #ax.set_xlabel('Error Type', fontsize = '40')
 ax.set_xticks(ind + width*2)
-ax.set_xticklabels(traces)
-ax.set_ylim([0, 7000])
+ax.set_xticklabels(traces, rotation = 45)
+ax.set_ylim([0, 18])
 ax.set_xlim([0, 6])
-plt.xticks(fontsize = '35')
-plt.yticks(fontsize = '40')
+plt.xticks(fontsize = '20')
+plt.yticks(fontsize = '25')
 
-ax.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 40}, ncol = 1)
+ax.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 25}, ncol = 1)
+ax.set_title("1 thread", fontsize = '35')
+
+ax1 = fig.add_subplot(132)
+rects = [[] for i in range(2)]
+for j in range(2):
+    name = 'rects' + str(j)
+#    rects[j] = ax.bar(ind + width * j, data[j], width, color=color_pattern[j % len(color_pattern)], hatch = hatch_pattern[j % len(hatch_pattern)])
+    rects[j] = ax1.bar(ind + width * (j + 1), data4[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
+
+# add some
+#ax1.set_ylabel('Bandwidth (MB/s)', fontsize = '40')
+#ax.set_xlabel('Error Type', fontsize = '40')
+ax1.set_xticks(ind + width*2)
+ax1.set_xticklabels(traces, rotation = 45)
+ax1.set_ylim([0, 18])
+ax1.set_xlim([0, 6])
+plt.xticks(fontsize = '20')
+plt.yticks(fontsize = '25')
+
+#ax1.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 40}, ncol = 1)
+ax1.set_title("4 threads", fontsize = '35')
+
+ax2 = fig.add_subplot(133)
+rects = [[] for i in range(2)]
+for j in range(2):
+    name = 'rects' + str(j)
+#    rects[j] = ax.bar(ind + width * j, data[j], width, color=color_pattern[j % len(color_pattern)], hatch = hatch_pattern[j % len(hatch_pattern)])
+    rects[j] = ax2.bar(ind + width * (j + 1), data16[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
+
+# add some
+#ax1.set_ylabel('Bandwidth (MB/s)', fontsize = '40')
+#ax.set_xlabel('Error Type', fontsize = '40')
+ax2.set_xticks(ind + width*2)
+ax2.set_xticklabels(traces, rotation = 45)
+ax2.set_ylim([0, 18])
+ax2.set_xlim([0, 6])
+plt.xticks(fontsize = '20')
+plt.yticks(fontsize = '25')
+
+#ax2.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 40}, ncol = 1)
+ax2.set_title("16 threads", fontsize = '35')
+
 plt.show()
