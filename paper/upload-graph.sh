9275308a32e033b086e461c958b6cbf2368a78e4
Andiry Xu <jix024@cs.ucsd.edu>
2014-06-25 16:36:06 -0700

    Paper script fix

--- paper/upload-graph.sh
+++ paper/upload-graph.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-scp -P 425 Graphs/* jix024@bb-00.calit2.net:~/trunk/Papers/2014SRC-TECHCON-Bankshot2/Graphs/
+#scp -P 425 Graphs/* jix024@bb-00.calit2.net:~/trunk/Papers/2014SRC-TECHCON-Bankshot2/Graphs/
+scp -P 425 Graphs/* jix024@bb-00.calit2.net:~/trunk/Papers/2015ASPLOS-Chell/Graphs/
+scp -P 425 Figures/* jix024@bb-00.calit2.net:~/trunk/Papers/2015ASPLOS-Chell/Figures/
