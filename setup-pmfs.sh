ebd91af03b24ea619ce78aae2454c817bc1dd3eb
Andiry Xu <jix024@cs.ucsd.edu>
2014-07-29 15:45:39 -0700

    PMFS setup fix

--- setup-pmfs.sh
+++ setup-pmfs.sh
@@ -6,8 +6,7 @@ modprobe pmfs
 
 sleep 1
 
-mount -t pmfs -o physaddr=0x100000000,init=2G none /mnt/ramdisk
+mount -t pmfs -o physaddr=0x100000000,init=8G none /mnt/ramdisk
 
-sleep 1
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
