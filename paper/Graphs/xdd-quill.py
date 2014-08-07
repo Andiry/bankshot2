7b03399f803b06f4760a30122c3d6b16663d7459
Andiry Xu <jix024@cs.ucsd.edu>
2014-08-07 02:55:21 -0700

    Graphs fix

--- paper/Graphs/xdd-quill.py
+++ paper/Graphs/xdd-quill.py
@@ -15,6 +15,7 @@ f = open(filename, 'r')
 round = 0
 types = []
 traces = []
+void = []
 data1 = [[] for i in range(2)]
 data4 = [[] for i in range(2)]
 data16 = [[] for i in range(2)]
@@ -27,6 +28,7 @@ for line in f:
 			types.append(parts[j])
 	elif int(parts[1]) == 1:
 		traces.append(parts[0])
+		void.append("")
 		for j in range(2,len(parts)):
 			data1[j - 2].append(float(parts[j]) / 1000)
 	elif int(parts[1]) == 4:
@@ -55,7 +57,7 @@ plt.rcParams['xtick.major.pad']='10'
 plt.rcParams['ytick.major.pad']='10'
 
 fig = plt.figure()
-ax = fig.add_subplot(131)
+ax = fig.add_subplot(311)
 rects = [[] for i in range(2)]
 color_pattern = ['1','0', '0.7', '0.3','0.6','0.9', '0.2', '0.5', '0.8', '0.1', '0.4','0.7', '1'] #light and contrasting colors
 hatch_pattern = ['\\','|','.','*','+','/', ' ', 'x', ' ', '-'] 
@@ -65,20 +67,21 @@ for j in range(2):
     rects[j] = ax.bar(ind + width * (j + 1), data1[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
 
 # add some
-ax.yaxis.labelpad = 20
-ax.set_ylabel('Bandwidth (GB/s)', fontsize = '25')
+#ax.yaxis.labelpad = 20
+ax.set_ylabel('Bandwidth (GB/s)', fontsize = '15')
 #ax.set_xlabel('Error Type', fontsize = '40')
-ax.set_xticks(ind + width*3)
-ax.set_xticklabels(traces, rotation = 45, ha = 'right')
-ax.set_ylim([0, 33])
-ax.set_xlim([0, 6])
-plt.xticks(fontsize = '15')
-plt.yticks(fontsize = '20')
+#ax.set_xticks(ind + width*3)
+ax.set_xticklabels(void, rotation = 45, ha = 'right')
+ax.set_ylim([0, 38])
+#ax.set_xlim([0, 6])
+#plt.xticks(fontsize = '15')
+plt.yticks(fontsize = '15')
 
-ax.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 20}, ncol = 1)
-ax.set_title("1 thread", fontsize = '25')
+ax.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 15}, ncol = 1)
+#ax.set_title("1 thread", fontsize = '15')
+ax.text(.5, .9, '1 thread', horizontalalignment = 'center', transform=ax.transAxes, fontsize = '15')
 
-ax1 = fig.add_subplot(132)
+ax1 = fig.add_subplot(312)
 rects = [[] for i in range(2)]
 for j in range(2):
     name = 'rects' + str(j)
@@ -86,19 +89,20 @@ for j in range(2):
     rects[j] = ax1.bar(ind + width * (j + 1), data4[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
 
 # add some
-#ax1.set_ylabel('Bandwidth (MB/s)', fontsize = '40')
+ax1.set_ylabel('Bandwidth (MB/s)', fontsize = '15')
 #ax.set_xlabel('Error Type', fontsize = '40')
-ax1.set_xticks(ind + width*3)
-ax1.set_xticklabels(traces, rotation = 45, ha = 'right')
-ax1.set_ylim([0, 33])
-ax1.set_xlim([0, 6])
-plt.xticks(fontsize = '15')
-plt.yticks(fontsize = '20')
+#ax1.set_xticks(ind + width*3)
+ax1.set_xticklabels(void, rotation = 45, ha = 'right')
+ax1.set_ylim([0, 38])
+#ax1.set_xlim([0, 6])
+#plt.xticks(fontsize = '15')
+plt.yticks(fontsize = '15')
 
 #ax1.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 40}, ncol = 1)
-ax1.set_title("4 threads", fontsize = '25')
+#ax1.set_title("4 threads", fontsize = '15')
+ax1.text(.5, .9, '4 threads', horizontalalignment = 'center', transform=ax1.transAxes, fontsize = '15')
 
-ax2 = fig.add_subplot(133)
+ax2 = fig.add_subplot(313)
 rects = [[] for i in range(2)]
 for j in range(2):
     name = 'rects' + str(j)
@@ -106,16 +110,17 @@ for j in range(2):
     rects[j] = ax2.bar(ind + width * (j + 1), data16[j], width, color=color_pattern[j], hatch = hatch_pattern[j % len(hatch_pattern)])
 
 # add some
-#ax1.set_ylabel('Bandwidth (MB/s)', fontsize = '40')
+ax2.set_ylabel('Bandwidth (MB/s)', fontsize = '15')
 #ax.set_xlabel('Error Type', fontsize = '40')
 ax2.set_xticks(ind + width*3)
 ax2.set_xticklabels(traces, rotation = 45, ha = 'right')
-ax2.set_ylim([0, 33])
+ax2.set_ylim([0, 38])
 ax2.set_xlim([0, 6])
 plt.xticks(fontsize = '15')
-plt.yticks(fontsize = '20')
+plt.yticks(fontsize = '15')
 
 #ax2.legend((rects[0][0], rects[1][0]), types, loc = "upper right", prop = {'size': 40}, ncol = 1)
-ax2.set_title("16 threads", fontsize = '25')
+#ax2.set_title("16 threads", fontsize = '15')
+ax2.text(.5, .9, '16 threads', horizontalalignment = 'center', transform=ax2.transAxes, fontsize = '15')
 
 plt.show()
