aea0ad37216de3241fa08f01cbd97401728d8e13
Andiry Xu <jix024@cs.ucsd.edu>
2014-09-30 16:18:33 -0700

    get-paper.sh fix

--- get-paper.sh
+++ get-paper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #scp -P 425 jix024@bb-00.calit2.net:~/trunk/Papers/2014SRC-TECHCON-Bankshot2/paper.pdf paper/
-scp -P 425 jix024@bb-00.calit2.net:~/trunk/Papers/2015ASPLOS-Chell/paper.pdf paper/
+scp -P 425 jix024@bb-00.calit2.net:~/trunk/Papers/2015FAST-Chell/submitted.pdf paper/
