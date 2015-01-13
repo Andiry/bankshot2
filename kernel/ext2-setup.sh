6f92e9a1e4044ea66819ff56f97135196d9868e6
Andiry Xu <jix024@cs.ucsd.edu>
2015-01-13 12:09:26 -0800

    Ext2 XIP mount

--- kernel/ext2-setup.sh
+++ kernel/ext2-setup.sh
@@ -5,7 +5,7 @@ mkfs.ext2 -b 4096 /dev/ram1
 
 sleep 1
 echo "Mount to /mnt/ramdisk1.."
-mount -o noatime /dev/ram1 /mnt/ramdisk
+mount -o xip /dev/ram1 /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
