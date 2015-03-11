cdc602fdf4e9f7fc85acfafc8b6ffaa1618bf386
Andiry Xu <jix024@cs.ucsd.edu>
2015-03-10 17:50:22 -0700

    New Graph result and scripts

---
+++ paper/Graphs/filebench1.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+if len(sys.argv) < 2:
+	print "Parameter Error"
+	exit(0)
+
+filename = sys.argv[1]
+f = open(filename, 'r')
+
+round = 0
+workloads = [[] for i in range(3)]
+traces = []
+data128=[]
+data512=[]
+data2048=[]
+types=[]
+i = 0
+
+for line in f:
+	line = line.rstrip()
+	parts = line.split('\t')
+	if round == 0:
+		i == 1
+	elif parts[1] == '128kB':
+		data128.append(float(parts[3]) / float(parts[2]))
+	elif parts[1] == '512kB':
+		data512.append(float(parts[3]) / float(parts[2]))
+	elif parts[1] == '2MB':
+		data2048.append(float(parts[3]) / float(parts[2]))
+	if round % 3 == 1:
+		types.append(parts[1])
+	if round > 0 and round < 4:
+		traces.append(parts[0])
+	round += 1
+
+file.close(f)
+print traces
+print data128
+print data512
+print data2048
+
+
+
+N = len(data128)
+ind = np.arange(N)
+width = 0.1
+
+print ind
+
+plt.rcParams['xtick.major.pad']='10'
+plt.rcParams['ytick.major.pad']='20'
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+rects = [[] for i in range(3)]
+color_pattern = ['0.9','0.6','0.3','#eeefff', 'c', 'b', '1','0.6', '0.2', '0.3','0.6','0.9', '0.2', '0.5', '0.8', '0.1', '0.4','0.7', '1'] #light and contrasting colors
+hatch_pattern = ['','','','\\','/','x','*','+','/', ' ', 'x', ' ', '-'] 
+
+rects[0] = ax.bar(ind + width * (0 + 3.5), data128, width, color=color_pattern[0], hatch = hatch_pattern[0 % len(hatch_pattern)])
+rects[1] = ax.bar(ind + width * (1 + 3.5), data512, width, color=color_pattern[1], hatch = hatch_pattern[1 % len(hatch_pattern)])
+rects[2] = ax.bar(ind + width * (2 + 3.5), data2048, width, color=color_pattern[2], hatch = hatch_pattern[2 % len(hatch_pattern)])
+ax.plot((0,3),(1,1),'k-')
+
+# add some
+ax.yaxis.labelpad = 30
+ax.set_ylabel('Normalized Ops per second', fontsize = '30')
+#ax.set_xlabel('Error Type', fontsize = '40')
+ax.set_xticks(ind + width*5)
+ax.set_xticklabels(traces)
+ax.set_ylim([0, 7])
+ax.set_xlim([0, 3])
+plt.xticks(fontsize = '30')
+plt.yticks(fontsize = '30')
+
+ax.legend((rects[0][0], rects[1][0], rects[2][0]), types, loc = "upper left", prop = {'size': 30}, ncol = 2)
+plt.show()
