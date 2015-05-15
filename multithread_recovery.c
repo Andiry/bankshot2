8e59220a9971abae47b06f5c081312bf19e8b8f0
Andiry Xu <jix024@cs.ucsd.edu>
2015-05-15 15:37:58 -0700

    Add timing for recovery

--- multithread_recovery.c
+++ multithread_recovery.c
@@ -18,11 +18,23 @@ struct malloc_request {
 int main(int argc, char *argv[])
 {
 	int fd1;
-	struct malloc_request req;
+	int multithread;
+	struct timespec start, end;
+
+	if (argc < 2) {
+		printf("Usage: ./multithread_recovery $MULTITHREAD\n");
+		return 0;
+	}
+
+	multithread = atoi(argv[1]);
 
 	fd1 = open("/mnt/ramdisk/test1", O_RDWR | O_CREAT, 0640);
 
-	ioctl(fd1, PMFS_TEST_MULTITHREAD_RECOVERY, &req);
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	ioctl(fd1, PMFS_TEST_MULTITHREAD_RECOVERY, &multithread);
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	printf("%s recovery time: %lu\n", multithread ?
+		"Multithread" : "Singlethread", end.tv_nsec - start.tv_nsec);
 
 	close(fd1);
 
